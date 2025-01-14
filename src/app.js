ca9c549705717cdcc1c9c060c3ee3a496d5459e7
gabriel-vieira <gabrielvieira040@outlook.com]>
2025-01-14 15:45:46 -0300

    feat: rmeovido rota para testes e adicionado rota para autentificação

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
+const app = express();
 const authRoutes = require('./routes/autentificacaoRota');
 
-const app = express();
-
 app.use(express.json());
+app.use(cors());
+
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Conectado'))
+    .catch(err => console.error('Erro conexão', err));
+
 app.use('/auth', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
 
 module.exports = app;
